a5e3c9bc1addc99c4bc33a5137947c805bc40e84
Sanrvallejo <zanti-23@hotmail.com>
2025-06-12 22:19:34 -0500

    fix: menu options interface in gifs-side-menu-options component

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { MenuOptions } from '../../../interfaces/menu-options.interface';
 
 @Component({
   selector: 'gifs-side-menu-options',
   imports: [],
   templateUrl: './gifs-side-menu-options.component.html',
 })
-export class GifsSideMenuOptionsComponent { }
+export class GifsSideMenuOptionsComponent {
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      sublabel: 'Popular Gifs',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      route: '/dashboard/search',
+      sublabel: 'Search gifs',
+    },
+  ];
+}
--- src/app/gifs/components/gifs-side-menu/gifs-side-menu.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu.component.ts
@@ -1,30 +1,10 @@
 import { Component } from '@angular/core';
-import { GifsSideMenuHeaderComponent } from "./gifs-side-menu-header/gifs-side-menu-header.component";
-import { GifsSideMenuOptionsComponent } from "./gifs-side-menu-options/gifs-side-menu-options.component";
-import { MenuOptions } from '../../interfaces/menu-options.interface';
+import { GifsSideMenuHeaderComponent } from './gifs-side-menu-header/gifs-side-menu-header.component';
+import { GifsSideMenuOptionsComponent } from './gifs-side-menu-options/gifs-side-menu-options.component';
 
 @Component({
   selector: 'gifs-side-menu',
   imports: [GifsSideMenuHeaderComponent, GifsSideMenuOptionsComponent],
   templateUrl: './gifs-side-menu.component.html',
 })
-export class GifsSideMenuComponent { 
-
-  menuOptions: MenuOptions[] = [
-
-    {
-      icon: 'fa-solid fa-chart-line',
-      label: 'Trending',
-      route: '/dashboard/trending',
-      sublabel: 'Popular Gifs'
-    },
-    {
-      icon: 'fa-solid fa-magnifying-glass',
-      label: 'Search',
-      route: '/dashboard/search',
-      sublabel: 'Search gifs'
-    }
-
-  ]
-  
-}
+export class GifsSideMenuComponent {}
