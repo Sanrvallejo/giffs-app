89cdfe7bf2c7109f97530f47317b724499d30f37
Sanrvallejo <zanti-23@hotmail.com>
2025-06-12 22:08:47 -0500

    add: menu options interface

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { GifsSideMenuHeaderComponent } from "./gifs-side-menu-header/gifs-side-menu-header.component";
 import { GifsSideMenuOptionsComponent } from "./gifs-side-menu-options/gifs-side-menu-options.component";
+import { MenuOptions } from '../../interfaces/menu-options.interface';
 
 @Component({
   selector: 'gifs-side-menu',
@@ -9,5 +10,21 @@ import { GifsSideMenuOptionsComponent } from "./gifs-side-menu-options/gifs-side
 })
 export class GifsSideMenuComponent { 
 
+  menuOptions: MenuOptions[] = [
+
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      sublabel: 'Popular Gifs'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      route: '/dashboard/search',
+      sublabel: 'Search gifs'
+    }
+
+  ]
   
 }
