02418fc57b16a21ff99b42108fa61862f8bf1c3b
Sanrvallejo <zanti-23@hotmail.com>
2025-06-17 23:43:15 -0500

    feat: searchGif method

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,12 +4,12 @@ import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { environment } from '@environments/environment';
 import { GifMapper } from '../mapper/gif.mapper';
 import { Gif } from '../interfaces/gif.interfaces';
+import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
-
   private http = inject(HttpClient);
 
   trendingGifs = signal<Gif[]>([]);
@@ -19,20 +19,32 @@ export class GifsService {
   }
 
   loadTrendingGifs() {
-    this.http.get<GiphyResponse>(`${environment.giphyBaseUrl}/gifs/trending`, {
+    this.http
+      .get<GiphyResponse>(`${environment.giphyBaseUrl}/gifs/trending`, {
         params: {
           api_key: environment.giphyApiKey,
-          limit: 20
-        }
-      }
-    )
-    .subscribe( (resp) => {
-      const gifs = GifMapper.mapGiphyItemArrayToGif(resp.data);
-      this.trendingGifs.set(gifs);
-      console.log(gifs);
-      
-      
-    } )
+          limit: 20,
+        },
+      })
+      .subscribe((resp) => {
+        const gifs = GifMapper.mapGiphyItemArrayToGif(resp.data);
+        this.trendingGifs.set(gifs);
+        console.log(gifs);
+      });
   }
 
+  searchGifs(query: string) {
+    return this.http
+      .get<GiphyResponse>(`${environment.giphyBaseUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          q: query,
+          limit: 20,
+        },
+      })
+      .pipe(
+        map(({ data }) => data),
+        map((items) => GifMapper.mapGiphyItemArrayToGif(items))
+      );
+  }
 }
