267feb77af595490dd387caf4b9223937cbe22f3
Sanrvallejo <zanti-23@hotmail.com>
2025-06-17 23:44:35 -0500

    feat: search component with search a gif section

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gif.interfaces';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
 })
-export default class SearchPageComponent { }
+export default class SearchPageComponent {
+  gifService = inject(GifsService);
+
+  gif = signal<Gif[]>([]);
+
+  onSearch(query: string) {
+    console.log(query);
+    this.gifService.searchGifs(query).subscribe((resp) => {
+      this.gif.set(resp);
+    });
+  }
+}
